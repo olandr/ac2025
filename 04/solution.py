3efd3e33a57d029397884849f54c6927bf6876eb
olandr <sonomis1@gmail.com>
2025-12-04 13:45:03 +0100

    add sol 4.2

--- 04/solution.py
+++ 04/solution.py
@@ -45,11 +45,39 @@ def solution_01(data):
   return(count)
 
 def solution_02(data):
-  return(0)
+  data_new=data
+  count=0
+  new_count=1
+  while new_count>0:
+    new_count = 0
+    mat = [[0 for _ in range(len(data[1])+2)] for _ in range(len(data)+2)]
+    mat_box = [[0 for _ in range(len(data[1])+2)] for _ in range(len(data)+2)]
+    for i,row in enumerate(data):
+      for j,value in enumerate(row):
+        if value == "@":
+          mat[i][j] +=1
+          mat[i+1][j] +=1
+          mat[i+2][j] +=1
+          mat[i][j+1] +=1
+          mat[i][j+2] +=1
+          mat[i+1][j+2] +=1
+          mat[i+2][j+1] +=1
+          mat[i+2][j+2] +=1
+
+          mat_box[i+1][j+1] = 1
+    
+    for i,row in enumerate(mat_box):
+      for j,value in enumerate(row):
+        if value and mat[i][j] < 4:
+          new_count +=1
+          data_new[i-1] = data_new[i-1][:j-1]+"."+data_new[i-1][j:]
+    count += new_count
+  return(count)
 
 
-print("Solution 1 test: ",solution_01(sample_data))
-print("Solution 1: ",solution_01(data))  ## Solution 1:  xxx
+#print("Solution 1 test: ",solution_01(sample_data))
+print("Solution 1: ",solution_01(data))  ## Solution 1:  1478
+
 
 #print("Solution 2 test: ",solution_02(sample_data))
-#print("Solution 2: ",solution_02(data)) ## Solution 2:  xxx
+print("Solution 2: ",solution_02(data)) ## Solution 2:  9120
