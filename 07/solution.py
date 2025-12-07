1416cc820057e3b556370bebadfc61ccbea08cdf
olandr <sonomis1@gmail.com>
2025-12-07 10:10:42 +0100

    add sol 7.2

--- 07/solution.py
+++ 07/solution.py
@@ -41,11 +41,33 @@ def solution_01(data):
   return(count)
 
 def solution_02(data):
-  return(0)
+  count=0
+  idx_lst=set()
+  count_lst={}
+  for row in data:
+    for idx,value in enumerate(row):
+      if value == "S":
+        idx_lst.add(idx)
+        count_lst[str(idx)]=1
+      elif value == "^" and count_lst.get(str(idx)):
+
+        if count_lst.get(str(idx-1)):
+          count_lst[str(idx-1)]=count_lst[str(idx-1)]+count_lst[str(idx)]
+        else:
+          count_lst[str(idx-1)]=count_lst[str(idx)]
+
+        if count_lst.get(str(idx+1)):
+          count_lst[str(idx+1)]=count_lst[str(idx+1)]+count_lst[str(idx)]
+        else:
+          count_lst[str(idx+1)]=count_lst[str(idx)]
+
+        count_lst.pop(str(idx))
+  
+  return(sum(count_lst.values()))
 
 
 print("Solution 1 test: ",solution_01(sample_data))
-print("Solution 1: ",solution_01(data))  ## Solution 1:  1613
+#print("Solution 1: ",solution_01(data))  ## Solution 1:  1613
 
 #print("Solution 2 test: ",solution_02(sample_data))
-#print("Solution 2: ",solution_02(data)) ## Solution 2:  xxx
+print("Solution 2: ",solution_02(data)) ## Solution 2:  48021610271997
